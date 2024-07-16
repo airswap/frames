863bbb49db0ccc0240c694f1e82a7564ca9d2b6a
Mike Starr <imhs@protonmail.com>
2024-07-16 17:13:24 +0200

    fixed state issue by consolodating frames into 1 file

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import { serveStatic } from '@hono/node-server/serve-static'
 import { Button, Frog, TextInput } from 'frog'
 import { devtools } from 'frog/dev'
+import { neynar } from 'frog/hubs'
 
 type State = {
   otcUrl: string | undefined
@@ -8,6 +9,9 @@ type State = {
 
 export const app = new Frog<{ State: State }>({
   title: "AirSwap OTC URL Frame",
+  // API key below is prone to rate limiting
+  hub: neynar({ apiKey: 'NEYNAR_FROG_FM' }),
+  verify: false,
   initialState: {
     otcUrl: undefined
   }
@@ -19,57 +23,60 @@ const airswapId = 'https://swap.eth.limo/#/order/'
 app.use('/*', serveStatic({ root: './public' }))
 
 app.frame('/', (c) => {
-  const { inputText, status, deriveState } = c
+  const { inputText, deriveState } = c
 
   deriveState(previousState => {
     if (inputText?.includes(airswapId)) {
       previousState.otcUrl = inputText}
     })
 
-  const otcUrl = inputText || undefined
-
   return c.res({
+    action: '/otcurl',
     image: (
       <div
         style={{
-          alignItems: 'center',
-          background:
-            status === 'response'
-              ? '#2c70ff'
-              : '#030712',
-          backgroundSize: '100% 100%',
           display: 'flex',
           flexDirection: 'column',
-          flexWrap: 'nowrap',
+          alignItems: 'center',
+          background: '#030712',
+          color: 'white',
           height: '100%',
           justifyContent: 'center',
           textAlign: 'center',
           width: '100%',
         }}
       >
-        <div
-          style={{
-            color: 'white',
-            fontSize: 60,
-            fontStyle: 'normal',
-            letterSpacing: '-0.025em',
-            lineHeight: 1.4,
-            marginTop: 30,
-            padding: '0 120px',
-            whiteSpace: 'pre-wrap',
-          }}
-        >
-          {status === 'response' && otcUrl
-            ? `${otcUrl}`
-            : 'Welcome AirSwap OTC Maker!'}
-        </div>
+        Welcome AirSwap OTC Maker!
       </div>
     ),
     intents: [
       <TextInput placeholder="Enter OTC URL..." />,
-      <Button action="/otcurl">Upload OTC URL</Button>
+      <Button>Upload OTC URL</Button>
     ],
   })
 })
 
+app.frame('/otcurl', (c) => {
+  return c.res({
+    image: (
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          background: '#030712',
+          color: 'white',
+          fontSize: '24px',
+          height: '100%',
+          justifyContent: 'center',
+          textAlign: 'center',
+          width: '100%',
+        }}>
+          {`OTC URL: ${c.inputText}`}
+      </div>
+    )
+  })
+})
+
 devtools(app, { serveStatic })
+
