71987d6c76ee52f30f146b24deec05a1cf7daf4c
Mike Starr <imhs@protonmail.com>
2024-07-16 20:44:53 +0200

    added additional frame for order details. Working on state manipulation

--- src/index.tsx
+++ src/index.tsx
@@ -1,37 +1,33 @@
-import { serveStatic } from '@hono/node-server/serve-static'
-import { Button, Frog, TextInput } from 'frog'
-import { devtools } from 'frog/dev'
-import { neynar } from 'frog/hubs'
+import { serveStatic } from '@hono/node-server/serve-static';
+import { Button, Frog, TextInput } from 'frog';
+import { devtools } from 'frog/dev';
+
+const airswapUrl = 'https://swap.eth.limo/#/order/';
 
 type State = {
-  otcUrl: string | undefined
-}
+  otcUrl: string | undefined;
+  orderDetails: string | undefined;
+};
 
 export const app = new Frog<{ State: State }>({
-  title: "AirSwap OTC URL Frame",
-  // API key below is prone to rate limiting
-  hub: neynar({ apiKey: 'NEYNAR_FROG_FM' }),
-  verify: false,
+  title: 'AirSwap OTC URL Frame',
   initialState: {
-    otcUrl: undefined
-  }
-})
+    otcUrl: undefined,
+  },
+});
 
-// const airswapId = 'https://swap.eth.limo/#/order'
-const airswapId = 'https://swap.eth.limo/#/order/'
-
-app.use('/*', serveStatic({ root: './public' }))
-
+app.use('/*', serveStatic({ root: './public' }));
 app.frame('/', (c) => {
-  const { inputText, deriveState } = c
-
-  deriveState(previousState => {
-    if (inputText?.includes(airswapId)) {
-      previousState.otcUrl = inputText}
-    })
+  const { inputText, deriveState } = c;
+  deriveState((previousState) => {
+    if (inputText?.includes(airswapUrl)) {
+      previousState.otcUrl = inputText;
+    } else {
+      previousState.otcUrl = 'invalid URL';
+    }
+  });
 
   return c.res({
-    action: '/otcurl',
     image: (
       <div
         style={{
@@ -44,19 +40,29 @@ app.frame('/', (c) => {
           justifyContent: 'center',
           textAlign: 'center',
           width: '100%',
+          fontSize: '30px',
         }}
       >
         Welcome AirSwap OTC Maker!
+        {/* <br /> */}
+        {/* {inputText && !validInput ? <span style={{color: '#ff0101'}}>Please enter a valid OTC URL...</span> : undefined} */}
       </div>
     ),
     intents: [
       <TextInput placeholder="Enter OTC URL..." />,
-      <Button>Upload OTC URL</Button>
+      <Button action="/orderDetails">Upload OTC URL</Button>,
     ],
-  })
-})
+  });
+});
 
-app.frame('/otcurl', (c) => {
+app.frame('/orderDetails', (c) => {
+  const { inputText, deriveState, previousState } = c;
+  deriveState((previousState) => {
+    if (inputText) {
+      previousState.orderDetails = inputText;
+    }
+  });
+
   return c.res({
     image: (
       <div
@@ -66,17 +72,56 @@ app.frame('/otcurl', (c) => {
           alignItems: 'center',
           background: '#030712',
           color: 'white',
-          fontSize: '24px',
           height: '100%',
           justifyContent: 'center',
           textAlign: 'center',
           width: '100%',
-        }}>
-          {`OTC URL: ${c.inputText}`}
+          fontSize: '30px',
+        }}
+      >
+        {`OTC URL: ${previousState.otcUrl}`}
+        <br />
+        {`(Enter order description below, e.g. "Swap 50 ETH for 200,000 USDC")`}
       </div>
-    )
-  })
-})
+    ),
+    intents: [
+      <TextInput placeholder="Enter order description..." />,
+      <Button action="/otcurl">Upload Order Details</Button>,
+      <Button.Reset>Back</Button.Reset>,
+    ],
+  });
+});
 
-devtools(app, { serveStatic })
+app.frame('/otcurl', (c) => {
+  const { previousState } = c;
+  return c.res({
+    image: (
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          background: '#030712',
+          color: 'white',
+          height: '100%',
+          justifyContent: 'center',
+          textAlign: 'center',
+          width: '100%',
+          fontSize: '30px',
+        }}
+      >
+        {previousState.orderDetails}
+        <br />
+        {`(clicking this frame will redirect you to AirSwap to fill this order`}
+      </div>
+    ),
+    intents: [
+      <Button.Link href={previousState.otcUrl ? previousState.otcUrl : '/'}>
+        Share
+      </Button.Link>,
+      <Button.Reset>Start over</Button.Reset>,
+    ],
+  });
+});
 
+devtools(app, { serveStatic });
