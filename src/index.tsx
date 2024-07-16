98e872aba1c73986f1a6b44765fb20bf31b37b11
Mike Starr <imhs@protonmail.com>
2024-07-16 21:31:39 +0200

    fixed state sharing across all frames

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import { serveStatic } from '@hono/node-server/serve-static';
 import { Button, Frog, TextInput } from 'frog';
 import { devtools } from 'frog/dev';
 
-const airswapUrl = 'https://swap.eth.limo/#/order/';
+const airswapOTCUrl = 'https://swap.eth.limo/#/order/';
 
 type State = {
   otcUrl: string | undefined;
@@ -13,20 +13,12 @@ export const app = new Frog<{ State: State }>({
   title: 'AirSwap OTC URL Frame',
   initialState: {
     otcUrl: undefined,
+    orderDetails: undefined,
   },
 });
 
 app.use('/*', serveStatic({ root: './public' }));
 app.frame('/', (c) => {
-  const { inputText, deriveState } = c;
-  deriveState((previousState) => {
-    if (inputText?.includes(airswapUrl)) {
-      previousState.otcUrl = inputText;
-    } else {
-      previousState.otcUrl = 'invalid URL';
-    }
-  });
-
   return c.res({
     image: (
       <div
@@ -44,8 +36,7 @@ app.frame('/', (c) => {
         }}
       >
         Welcome AirSwap OTC Maker!
-        {/* <br /> */}
-        {/* {inputText && !validInput ? <span style={{color: '#ff0101'}}>Please enter a valid OTC URL...</span> : undefined} */}
+        <br /> Enter your OTC URL below{' '}
       </div>
     ),
     intents: [
@@ -56,10 +47,12 @@ app.frame('/', (c) => {
 });
 
 app.frame('/orderDetails', (c) => {
-  const { inputText, deriveState, previousState } = c;
+  const { inputText, deriveState } = c;
   deriveState((previousState) => {
-    if (inputText) {
-      previousState.orderDetails = inputText;
+    if (inputText?.includes(airswapOTCUrl)) {
+      previousState.otcUrl = inputText;
+    } else {
+      previousState.otcUrl = 'invalid OTC URL';
     }
   });
 
@@ -79,13 +72,11 @@ app.frame('/orderDetails', (c) => {
           fontSize: '30px',
         }}
       >
-        {`OTC URL: ${previousState.otcUrl}`}
-        <br />
-        {`(Enter order description below, e.g. "Swap 50 ETH for 200,000 USDC")`}
+        {`Now enter order description below, e.g. "Swap 50 ETH for 200,000 USDC"`}
       </div>
     ),
     intents: [
-      <TextInput placeholder="Enter order description..." />,
+      <TextInput placeholder="Now enter order description..." />,
       <Button action="/otcurl">Upload Order Details</Button>,
       <Button.Reset>Back</Button.Reset>,
     ],
@@ -93,6 +84,42 @@ app.frame('/orderDetails', (c) => {
 });
 
 app.frame('/otcurl', (c) => {
+  const { inputText, deriveState, previousState } = c;
+  deriveState((previousState) => {
+    previousState.orderDetails = inputText;
+  });
+
+  return c.res({
+    image: (
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          background: '#030712',
+          color: 'white',
+          height: '100%',
+          justifyContent: 'center',
+          textAlign: 'center',
+          width: '100%',
+          fontSize: '30px',
+        }}
+      >
+        {previousState.otcUrl}
+        <br />
+        {inputText}
+        <br />
+        {`(Click "Share" button below to make this frame sharable with your followers. Otherwise click "Start over")`}
+      </div>
+    ),
+    intents: [
+      <Button action="/sharable">Share</Button>,
+      <Button.Reset>Start over</Button.Reset>,
+    ],
+  });
+});
+
+app.frame('/sharable', (c) => {
   const { previousState } = c;
   return c.res({
     image: (
@@ -112,14 +139,14 @@ app.frame('/otcurl', (c) => {
       >
         {previousState.orderDetails}
         <br />
-        {`(clicking this frame will redirect you to AirSwap to fill this order`}
+
+        {`(Click "Fill order" to be redirected to AirSwap to take this OTC order!`}
       </div>
     ),
     intents: [
       <Button.Link href={previousState.otcUrl ? previousState.otcUrl : '/'}>
-        Share
+        Fill order
       </Button.Link>,
-      <Button.Reset>Start over</Button.Reset>,
     ],
   });
 });
